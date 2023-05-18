f64bf22e8e916b203424a1ed8528a0b8a39a1c8c
Scharly OPITZ <scharly.opitz@gmail.com>
2023-05-18 12:14:46 +0200

    Variable haut de page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 
 export default function Card({ id, cover, title }) {
+    // Variable pour se mettre en haut de la page au clic
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+        });
+    };
+
     return (
-        <Link to={`/logement/${id}`} className="card">
+        <Link to={`/logement/${id}`} onClick={goToTop} className="card">
             <img src={cover} alt="image de couverture" className="cover" />
             <div className="card-effect"></div>
             <p>{title}</p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,26 @@ import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
 
 export default function Header() {
+    // Variable pour se mettre en haut de la page au clic
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+        });
+    };
+
     return (
         <div className="header">
             <img src={logo} alt="logo Kasa" />
             <ul>
                 <li>
-                    <NavLink to="/">Acceuil</NavLink>
+                    <NavLink to="/" onClick={goToTop}>
+                        Acceuil
+                    </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/about">A Propos</NavLink>
+                    <NavLink to="/about" onClick={goToTop}>
+                        A Propos
+                    </NavLink>
                 </li>
             </ul>
         </div>
